5416a7f8d08062610420ac6df7948097cbc5d6fa
yuanjia-h <123715364+yuanjia-h@users.noreply.github.com>
2023-11-14 09:51:38 +0800

    multiplication_table.java

---
+++ multiplication_table.java
@@ -0,0 +1,12 @@
+public class multiplication_table {
+    public static void main(String[] args) {
+        for (int i=1;i<=9;i++){
+            for (int j=1;j<=i;j++){
+                int sum=i*j;
+                System.out.print(i+"*"+j+"="+sum+"\t");
+
+            }
+            System.out.println("  ");
+        }
+    }
+}
